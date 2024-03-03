b1f3ec5b57a9363f523bc09bb5fe3b67ccd0bab0
6rm6n <74957929+6rm6n@users.noreply.github.com>
2024-03-03 09:05:11 -0500

    Update main.py
    backend stuff

--- rare-connect-backend/main.py
+++ rare-connect-backend/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+import csv
+#import numpy as np
 
 app = FastAPI()
+csv_file_path = "./RareDiseases.csv"
 
 
 @app.post("/questionaire")
-async def questionaire(symptoms: str):
-    symptomsList = symptoms.split(", ")
-    return {"symptoms": symptomsList}
+def getSymptomsCsv():
+    symptomArray = {}
+    with open(csv_file_path, mode = 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader: # cycles through possible diseases
+            #print(row[1])
+            symptomArray.append(row[1].split(", "))
+    return symptomArray
+
+async def findSimilarSymptoms(userSymptoms: str):
+    symptomScores = [0] * 14
+    symptomMaxScores = [0] * 14
+    diseaseSymptoms = getSymptomsCsv()
+    
+
+print(getSymptomsCsv())
+        #for() # cycle through the symptoms for possible diseases, split by commas
+        # compare symptoms with the user's symptoms
